a4a9ba941108a582dfb310e504324112982baf5b
D.Tau <di.tau9@gmail.com>
2021-04-06 22:29:55 +0300

    настроил webpack, добавил реакт-компоненты портфолио

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../assets/global.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <h1>SaLuT WEBPACK!!! !!</h1>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+let path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let conf = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: '/dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
+
+module.exports = (env, options) => {
+  let isProd = options.mode === 'production';
+  conf.devtool = isProd ? false : 'eval-cheap-module-source-map'
+  // пока не пофиксят баг с webpack-dev-server (в 4 версии должны испрвить)
+  conf.target = isProd ? 'browserslist' : 'web'
+  return conf;
+};
+
